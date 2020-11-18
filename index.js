08ea88c3e51f9dd3f9c4e5935bbe7bbcf5de4ee4
jonatas401 <jonnatas56@hotmail.com>
2020-11-18 17:24:52 -0300

    nova banch

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ const express = require('express');
 const app = express();
 
 
+app.get('/', (req, res)=>{
+    res.send('<h1>projeto<h1>')
+})
+
+
+
 
 app.listen(3000,'0.0.0.0', ()=>{
  console.log('sevidor rodando na porta 3000')
