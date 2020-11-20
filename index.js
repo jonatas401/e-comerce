791963b2e17682f73c4e6969f01e98e8dfcc8670
jonatas401 <jonnatas56@hotmail.com>
2020-11-20 04:03:17 -0300

    tela de cadastro

--- index.js
+++ index.js
@@ -1,14 +1,33 @@
+//importando express*****
 const express = require('express');
 const app = express();
+//importando marko tamplate-----------------
+require('marko/node-require').install();
+const markoExpress = require('marko/express');
+app.use(markoExpress());
+//paginas estaticas ------------
+app.use('/static', express.static('public'));
+app.use(express.static('public'));
+//cors ----------------
+const cors = require('cors');
+app.use(cors());
 
+//importando o bodyparser --------------------
+var bodyParser = require('body-parser')
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.get('/', (req, res)=>{
-    res.send('<h1>projeto<h1>')
+    res.marko(require('./view/main.marko'))
+})
+app.get('/cadastro', (req,res)=>{
+    
+    res.marko(require('./view/cadastro.marko'))
+    res.json({msg: ' o cors está ligado'})
 })
 
 
 
 
-app.listen(3000,'0.0.0.0', ()=>{
- console.log('sevidor rodando na porta 3000')
+app.listen(3001,'0.0.0.0', ()=>{
+ console.log('sevidor rodando na porta 3001')
 })
